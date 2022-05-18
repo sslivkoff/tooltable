4fddd6762e9b5d8e4f12c73014ced7e86776ecca
sslivkoff <stormslivkoff@gmail.com>
2022-05-18 13:23:13 -0700

    bump to 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='tooltable',
-    version='0.1.0',
+    version='0.1.1',
     packages=['tooltable'],
     install_requires=[
         'numpy',
--- tooltable/__init__.py
+++ tooltable/__init__.py
@@ -1,3 +1,3 @@
 from .table_io import *
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
